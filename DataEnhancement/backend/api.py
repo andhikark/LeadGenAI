cfc6e2655c53299131cbe022f64c679194279be5
Andhika_RK <andhikarestu873@gmail.com>
2025-04-22 13:35:54 +0700

    add port

--- DataEnhancement/backend/api.py
+++ DataEnhancement/backend/api.py
@@ -137,4 +137,6 @@ def scrape():
         scraper.close()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render will provide the port
+    app.run(host="0.0.0.0", port=port, debug=True)
+
